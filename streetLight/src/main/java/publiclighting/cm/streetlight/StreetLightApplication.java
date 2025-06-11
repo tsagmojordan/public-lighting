f7befbafc9eb1d0aa5b0ca77a8a9b78c883049cd
Jordan Tsagmo <jordantsagmo@gmail.com>
2025-06-11 15:29:55 +0100

    streetLight projet initialisation

---
+++ streetLight/src/main/java/publiclighting/cm/streetlight/StreetLightApplication.java
@@ -0,0 +1,13 @@
+package publiclighting.cm.streetlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetLightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreetLightApplication.class, args);
+    }
+
+}
---
+++ streetLight/src/test/java/publiclighting/cm/streetlight/StreetLightApplicationTests.java
@@ -0,0 +1,13 @@
+package publiclighting.cm.streetlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetLightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
